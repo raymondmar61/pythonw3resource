fdeddeb8d2c4b9f7b27daa2d855e342eea88832c
raymondmar61 <raym61@hotmail.com>
2017-11-20 20:46:09 -0800

    upload

--- basics.py
+++ basics.py
@@ -25,6 +25,12 @@ print(sys.version) #print 3.5.2 (default, Nov 17 2016, 17:05:23)\n [GCC 5.4.0 20
 #3. Write a Python program to display the current date and time.
 from datetime import datetime
 print(datetime.now()) #print 2017-11-10 20:12:11.688408
+currentdate = datetime.now()
+print(currentdate.month) #print 11
+print(currentdate.year) #print 2017
+print(currentdate.day) #print 20
+print(currentdate.month,"/",currentdate.day,"/",currentdate.year)
+print(currentdate.strftime("%m/%d/%Y")) #print 11/20/2017
 
 #4. Write a Python program which accepts the radius of a circle from the user and compute the area.
 from math import pi
@@ -47,3 +53,41 @@ print(sampledata) #print (3, 5, 7, 23)
 # print(sampledatalist) #print ['3', '5', '7', '23']
 print(tuple(map(str,sampledata))) #print ('3', '5', '7', '23')
 print(list(map(str,sampledata))) #print ['3', '5', '7', '23']
+
+#7. Write a Python program to accept a filename from the user and print the extension of that.  RM:  skipped accept a filename from the user
+import os
+filename, fileextension = os.path.splitext("/home/mar/Python/socratica/googlestockdata.csv")
+print(filename) #print /home/mar/Python/socratica/googlestockdata
+print(fileextension) #print .csv
+
+#8. Write a Python program to display the first and last colors from the following list. 
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0]) #print Red
+print(color_list[-1]) #print Black
+
+#9. Write a Python program to display the examination schedule. (extract the date from exam_st_date exam_st_date = (11, 12, 2014)  Sample Output : The examination will start from : 11 / 12 / 2014
+examstartdate = (11, 12, 2014)
+print(type(examstartdate)) #print <class 'tuple'>
+print(type(examstartdate[0])) #print <class 'int'>
+print("The examination will start from:", examstartdate[0],"/",examstartdate[1],"/",examstartdate[2]) #print The examination will start from: 11 / 12 / 2014
+# printexamestartdate = datetime.strptime(examstartdate,"%m/%d/%Y")
+# print(printexamestartdate) #error TypeError: strptime() argument 1 must be str, not tuple
+
+#10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. Sample value of n is 5. Expected Result : 615.
+n = 5
+print(n+(n**2)+(n**3)) #print 155
+a = 5
+n1 = int( "%s" % a )
+print(n1) #print 5
+n2 = int( "%s%s" % (a,a) )
+print(n2) #print 55
+n3 = int( "%s%s%s" % (a,a,a) )
+print(n3) #print 555
+print(n1+n2+n3) #print 615
+
+#11. Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+print(abs.__doc__)
+print(print.__doc__)
+print(type.__doc__)
+print(dir(__builtins__)) #['ArithmeticError', 'AssertionError', ...,  'type', 'vars', 'zip']
+#RM:  https://docs.python.org/3/library/functions.html website has all built-in functions
