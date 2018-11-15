2badff1e27e54c85562f0f64ae86c987c8b3f393
raymondmar61 <raym61@hotmail.com>
2018-11-14 21:24:25 -0800

    upload

--- pythonexercises.py
+++ pythonexercises.py
@@ -1,20 +1,10 @@
-#33. Write a Python program to generate all sublists of a list.
-from itertools import combinations
-# def generatesublists(primarylist):
-# 	sublist = []
-# 	sublist1only = []
-# 	itemcount = len(primarylist)
-# 	for n in range(0,itemcount+1):
-# 		# if n == 1:
-# 		# 	for eachprimarylist in primarylist:
-# 		# 		sublist1only.append(eachprimarylist)
-# 		# 	sublist.append(sublist1only)
-# 		# elif n > 1:
-# 		# 	output = list(combinations(primarylist,n))
-# 		# 	sublist.append(output)
-# 		output = list(combinations(primarylist,n))
-# 		sublist.append(output)
-# 	print(sublist)
-list1 = [10, 20, 30, 40]
-list2 = ['X', 'Y', 'Z']
-generatesublists(list1)
+#24. Write a Python program to create a dictionary from a string.  Note: Track the count of the letters from the string.  Sample string : 'w3resource'. Expected output: {'3': 1, 's': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
+my_dict = {'C1':[1,2,3],'C2':[5,6,7],'C3':[9,10,11]}
+for row in zip(*([key] + (value) for key, value in sorted(my_dict.items()))):
+    print(*row)
+'''
+C1 C2 C3
+1 5 9
+2 6 10
+3 7 11
+'''
