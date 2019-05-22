366c8fd60d35150ef40b07303b7a4b1578516d19
raymondmar61 <raym61@hotmail.com>
2019-05-21 20:14:45 -0700

    upload

--- mathpython.py
+++ mathpython.py
@@ -50,3 +50,43 @@ angleinradians = angle * ((pi)/180)
 arclength = radius*angleinradians
 print(arclength) #print 3.5342917352885173
 
+#8. Write a Python program to calculate the area of the sector.
+from math import pi
+radius = 4
+angle = 45
+area = pi*(radius**2)*(angle/360)
+print(area) #print 6.283185307179586
+
+#9. Write a Python program to calculate the discriminant value.
+#Source:  https://www.solumaths.com/en/calculator-online/calculate/discriminant  =(b**2)-(4*a*c)
+x = 4
+y = 0
+z = -4
+discriminant = (y**2)-(4*x*z)
+print(discriminant) #print 64
+
+#10. Write a Python program to find the smallest multiple of the first n numbers. Also, display the factors.
+#RM:  I don't understand the question.
+
+#11. Write a Python program to calculate the difference between the squared sum of first n natural numbers and the sum of squared first n natural numbers.(default value of number=2).
+n = 12
+squaredsum = 0
+sumsquared = 0
+for x in range(1,n+1):
+	squaredsum+=x
+	sumsquared = (sumsquared + (x**2))
+squaredsum = squaredsum**2
+print(squaredsum - sumsquared) #print 5434
+
+#12. Write a Python program to calculate the sum of all digits of the base to the specified power.
+#RM:  sum each digit in a number raised to a power; e.g. 2**100 is 1267650600228229401496703205376.  Sum each digit.
+def powerbasesum(base, power):
+	number = pow(base, power)
+	answersum = 0
+	for eachnumber in str(number):
+		answersum = answersum + int(eachnumber)
+	return answersum
+print(powerbasesum(2, 100)) #print 115
+print(powerbasesum(8, 10)) #print 37
+
+
